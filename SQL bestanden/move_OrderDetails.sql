7845b528c2cf0597f3786c77b828f7637971c804
ThomasB <60038450+ThomasBoyens@users.noreply.github.com>
2022-06-05 11:22:02 +0200

    Update move_OrderDetails.sql

--- SQL bestanden/move_OrderDetails.sql
+++ SQL bestanden/move_OrderDetails.sql
@@ -14,7 +14,6 @@ BEGIN
 	WHERE
 		(TRY_CAST (OrderID as int) IS NOT NULL) AND
 		(TRY_CAST (Quantity as TINYINT) IS NOT NULL) AND
-		-- Discount is overal null moet dit dan in de cleansed tabel of willen we alleen cijfers in de tabel
 		(TRY_CAST (Discount as decimal) IS NULL)
 
 	INSERT INTO [staging].[ARCHIVED].[Orders Details] ([OrderID],[Quantity],[Discount])
