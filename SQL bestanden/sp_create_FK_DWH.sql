df62852301f5f9e5620882bfe0fbd0376c70a6b8
Baeten Jens <61287853+dev-jens@users.noreply.github.com>
2022-06-05 13:30:33 +0200

    Update sp_create_FK_DWH.sql

--- SQL bestanden/sp_create_FK_DWH.sql
+++ SQL bestanden/sp_create_FK_DWH.sql
@@ -3,23 +3,23 @@ as
 begin
 	-- FK productID in FACT.orders
 	ALTER TABLE [SeaShippers_DWH].[FACT].[Orders] WITH NOCHECK
-		ADD CONSTRAINT FK_Product_ID FOREIGN KEY ([id])
-			REFERENCES [SeaShippers_DWH].[DIM].[Product]([id])
+		ADD CONSTRAINT FK_Product_ID FOREIGN KEY ([product_id])
+			REFERENCES [SeaShippers_DWH].[DIM].[Product]([Product_id])
+
+	-- FK CustomerID in FACT.orders
+	ALTER TABLE [SeaShippers_DWH].[FACT].[Orders] WITH NOCHECK
+		ADD CONSTRAINT FK_Customer_ID FOREIGN KEY ([Customer_id])
+			REFERENCES [SeaShippers_DWH].[DIM].[Customers]([Customer_id])
 
 	-- FK productID in FACT.orders
 	ALTER TABLE [SeaShippers_DWH].[FACT].[Orders] WITH NOCHECK
-		ADD CONSTRAINT FK_Customer_ID FOREIGN KEY ([id])
-			REFERENCES [SeaShippers_DWH].[DIM].[Customers]([id])
+		ADD CONSTRAINT FK_ShipDate_ID FOREIGN KEY ([Order_date])
+			REFERENCES [SeaShippers_DWH].[DIM].[ShipDate]([Order_date])
 
 	-- FK productID in FACT.orders
 	ALTER TABLE [SeaShippers_DWH].[FACT].[Orders] WITH NOCHECK
-		ADD CONSTRAINT FK_ShipDate_ID FOREIGN KEY ([id])
-			REFERENCES [SeaShippers_DWH].[DIM].[ShipDate]([id])
-
-	-- FK productID in FACT.orders
-	ALTER TABLE [SeaShippers_DWH].[FACT].[Orders] WITH NOCHECK
-		ADD CONSTRAINT FK_shipVia_ID FOREIGN KEY ([id])
-			REFERENCES [SeaShippers_DWH].[DIM].[Shippers]([id])
+		ADD CONSTRAINT FK_shipVia_ID FOREIGN KEY ([Shipvia])
+			REFERENCES [SeaShippers_DWH].[DIM].[Shippers]([Shipper_id])
 
 	-- FK productID in FACT.orders
 	ALTER TABLE [SeaShippers_DWH].[FACT].[Orders] WITH NOCHECK
